cdeebdfed2f513b6e7ec7dd2e84eeef597411060
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-02-03 14:31:13 +0000

    part a done

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -2,7 +2,10 @@ public class BoxOfCandy
 {
     /** box contains at least one row and is initialized in the constructor. */
     private Candy[][] box;
-
+    public BoxOfCandy(Candy[][] b)
+    {
+        box = b;
+    }
     /**
     * Moves one piece of candy in column col, if necessary and possible, so that the box
     * element in row 0 of column col contains a piece of candy, as described in part (a).
@@ -10,14 +13,38 @@ public class BoxOfCandy
     * Precondition: col is a valid column index in box.
     */
     public boolean moveCandyToFirstRow(int col)
-    { /* to be implemented in part (a) */ }
+    { 
+        if(box[0][col] != null) return true;
+        for(int i = 1; i < box.length; i++)
+        {
+            if (box[i][col] != null)
+            {
+                box[0][col] = box[i][col];
+                box[i][col] = null;
+                return true;
+            }
+        }
+        return false;
+    }
 
     /**
     * Removes from box and returns a piece of candy with flavor specified by the parameter, or
     * returns null if no such piece is found, as described in part (b)
     */
-    public Candy removeNextByFlavor(String flavor)
+ //   public Candy removeNextByFlavor(String flavor)
     { /* to be implemented in part (b) */ }
 
-    // There may be instance variables, constructors, and methods that are not shown.
+    public String toString()
+    {
+        String s = "";
+        for(int row = 0; row < box.length; row ++)
+        {
+            for(int col = 0; col < box[0].length; col ++)
+            {
+                s += box[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main{
     public static void main(String[] args){
         Candy[][] box = new Candy[4][3];
+        box[0][1] = new Candy("lime");
+        box[1][1] = new Candy("orange");
+        box[2][2] = new Candy("cherry");
+        box[3][1] = new Candy("lemon");
+        box[3][2] = new Candy("grape");
+        BoxOfCandy b = new BoxOfCandy(box);
+        System.out.println(b);
+        b.moveCandyToFirstRow(2);
+        System.out.println(b);
+        Candy[][] box2 = new Candy[3][5];
+        box2[0][0] = new Candy("lime");
+        box2[0][1] = new Candy("orange");
+        box2[0][3] = new Candy("cherry");
+        box2[1][0] = new Candy("lemon");
+        box2[1][3] = new Candy("grape");
+        box2[1][4] = new Candy("lime");
+        box2[2][0] = new Candy("orange");
+        box2[2][2] = new Candy("cherry");
+        box2[2][4] = new Candy("lemon");
+        BoxOfCandy b2 = new BoxOfCandy(box2);
     }
 }
